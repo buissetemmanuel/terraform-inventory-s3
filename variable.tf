04af7f0ffd63dace37ca93f3ff9af31e0185853a
buissetemmanuel <emmanuel@buisset.ch>
2024-05-19 15:01:13 +0200

    feat: initial commit

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "S3_ENDPOINT" {
+  sensitive = true
+}
+
+variable "S3_REGION_KEY" {
+  sensitive = true
+}
+
+variable "S3_ACCESS_KEY" {
+  sensitive = true
+}
+
+variable "S3_SECRET_KEY" {
+  sensitive = true
+}
